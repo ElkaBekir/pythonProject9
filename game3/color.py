5d5817a3a9cd7b9d12648590d2d131408e378f5d
Mr.Eldar <elka.bekir@gmail.com>
2023-03-25 18:56:00 -0400

    learningday9

---
+++ game3/color.py
@@ -0,0 +1,11 @@
+#colors = ["purple", "teal", "magenta", "crimson", "emerald"]
+
+#index = 0
+#while index < len(colors):
+    #print(f"{index}: {colors[index]}")
+   # index += 1
+
+colors = ["purple", "teal", "magenta"]
+
+for color in colors:
+    print(color)
---
+++ game3/nums.py
@@ -0,0 +1,16 @@
+nums = [1,2,3,4]
+nums.insert(len(nums), "LAST")
+print(nums)
+
+data = [1,2,3]
+
+print(data)
+
+data.append("purple")
+print(data)
+
+print(1+3)
+
+len("12312asddas")
+
+len([1])
